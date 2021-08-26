59fca36b02bf1308b7b766c53b187423a1059149
Paulo Rodolfo Barbosa Rodrigues <paauleras@gmail.com>
2021-08-26 12:36:42 -0300

    Versão 0.0.2
    - Melhorias na interface e experiência da extensão
    - Novas informações adicionadas: Level, Rank, Classe e Raça
    - Remoção de alguns códigos desnecessários

--- lookup.js
+++ lookup.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function myAlert(){
-  // alert('hello world');
   var valor = document.getElementById("tokenid_input").value;
   urlBine = valor;
   if(urlBine == null){
@@ -23,12 +22,65 @@ function myAlert(){
       bID = bine.response.binemon_item.tokenID;
       bThumb = bine.response.binemon_item.thumbnail;
       bSkills = bine.response.binemon_item.skills;
+      // Raça
+      bBreed = bine.response.binemon_item.breed;
+      // Classe de luta
+      bClass = bine.response.binemon_item.class;
+      bRank = bine.response.binemon_item.rank;
+      bLevel = bine.response.binemon_item.level;
+
+      if (bBreed == 1){
+        bBreed = '<img src="https://app.binemon.io/images/breed-dog.png" alt="Dog"/>';
+      }if (bBreed == 2){
+        bBreed = '<img src="https://app.binemon.io/images/breed-cat.png" alt="Cat"/>';
+      }if (bBreed == 3){
+        bBreed = '<img src="https://app.binemon.io/images/breed-titan.png" alt="Titan"/>';
+      }if (bBreed == 4){
+        bBreed = '<img src="https://app.binemon.io/images/breed-unicorn.png" alt="Unicorn"/>';
+      }
+      console.log(bBreed);
+
+      if (bClass == 1){
+        bClass = '<img src="https://app.binemon.io/images/dps-class.png" alt="Fighter" title="Fighter"/>';
+      }if (bClass == 2){
+        bClass = '<img src="https://app.binemon.io/images/tanker-class.png" alt="Tanker" title="Tanker"/>';
+      }if (bClass == 3){
+        bClass = '<img src="https://app.binemon.io/images/support-class.png" alt="Support" title="Support"/>';
+      }if (bClass == 4){
+        bClass = '<img src="https://app.binemon.io/images/mage-class.png" alt="Mage" title="Mage"/>';
+      }if (bClass == 5){
+        bClass = '<img src="https://app.binemon.io/images/archer-class.png" alt="Archer" title="Archer"/>';
+      }
+      console.log(bClass);
+
+      if (bRank == 1){
+        bRank = "E";
+      }if (bRank == 2){
+        bRank = "D";
+      }if (bRank == 3){
+        bRank = "C";
+      }if (bRank == 4){
+        bRank = "B";
+      }if (bRank == 5){
+        bRank = "A";
+      }if (bRank == 6){
+        bRank = "S";
+      }if (bRank == 7){
+        bRank = "SR";
+      }
+      console.log(bRank);
+      
       //referências no HTML
       document.getElementById("bName").innerHTML = bName;
       document.getElementById("bID").innerHTML = `(${bID})`;
       document.getElementById("thumb").innerHTML = `<img class="thumb" src="${bThumb}" />`;
       document.getElementById("bPassive").innerHTML = bSkills[0];
       document.getElementById("bUltimate").innerHTML = bSkills[1];
+      document.getElementById("bBreed").innerHTML = bBreed;
+      document.getElementById("bClass").innerHTML = bClass;
+      document.getElementById("bRank").innerHTML = bRank;
+      document.getElementById("bLevel").innerHTML = bLevel;
+
       
     })
   }
